bb682b6061ababb4e22ff5f2e520a4f5ee170f1a
yasir ahmed <yasira640@gmail.com>
2020-03-18 17:10:57 +0500

    / path in client

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'bootstrap';
-require('./index.scss');
-import Root from './components/Root.js';
+require('/index.scss');
+import Root from '/components/Root.js';
 
 if(document.getElementById('root')){
     ReactDOM.render(
