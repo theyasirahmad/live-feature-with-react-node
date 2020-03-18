687a792a949fa6fa7916bfaa0f846f695002cef6
yasir ahmed <yasira640@gmail.com>
2020-03-18 16:38:20 +0500

    ffmpeg

--- server/config/default.js
+++ server/config/default.js
@@ -17,7 +17,7 @@ const config = {
             allow_origin: '*'
         },
         trans: {
-            // ffmpeg: 'C:/Program Files/ffmpeg/bin/ffmpeg.exe',
+            // ffmpeg: 'C:/Program Files/ffmpeg/bin/ffmpeg',
             ffmpeg: 'ffmpeg',
             tasks: [
                 {
